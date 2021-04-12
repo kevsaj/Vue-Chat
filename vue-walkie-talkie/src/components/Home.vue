7d1aca4b3348b08b65bac6ec9229b1d152d02bff
Kevin Sajan <kevsaj@gmail.com>
2021-04-12 00:08:17 -0400

    Firebase auth

--- vue-walkie-talkie/src/components/Home.vue
+++ vue-walkie-talkie/src/components/Home.vue
@@ -1,5 +1,17 @@
 <template>
     <div>
         <h3>Home</h3>
+
+        <Login />
     </div>
 </template>
+
+<script>
+import Login from './Login'
+
+export default ({
+    components: {
+        Login
+    }
+})
+</script>
