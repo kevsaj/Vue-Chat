9853bca554f8c49277918d73dfaef18443a46c5c
Kevin Sajan <kevsaj@gmail.com>
2021-04-14 00:04:20 -0400

    email auth working

--- vue-walkie-talkie/src/components/Login.vue
+++ vue-walkie-talkie/src/components/Login.vue
@@ -1,5 +1,5 @@
 <template>
-    <aside>
+    <aside class="section">
         <h3>Sign in Anonymously</h3>
         <button @click="auth.signInAnonymously()" class="button is-dark">Sign In</button>
 
@@ -10,20 +10,22 @@
 
         <div v-else>
             <h3>Sign In with Email</h3>
-            <a href="#" @click="newUser = true"></a>
+            <a href="#" @click="newUser = true">New User?</a>
         </div>
 
-        <label for="email">Email</label><br>
+        <label for="email">Email</label><br />
         <input v-model="email" placeholder="email" type="email" class="input">
 
-        <label for="password">Password</label><br>
+        <label for="password">Password</label><br />
         <input v-model="password" type="password" class="input">
 
-        <br>
+        <br />
 
-        <button>
+        <button class="button is-info" :class="{ 'is-loading': loading }" @click="signInOrCreateUser()">
             {{ newUser ? 'Sign Up' : 'Login' }}
         </button>
+
+        <p class="has-text-danger" v-if="errorMessage">{{ errorMessage }}</p>
 
     </aside>
 </template>
@@ -34,9 +36,31 @@ import { auth } from '../firebase'
 export default ({
     data() {
         return {
+            auth,
             newUser: false,
             email: '',
             password: '',
+            loading: false,
+            errorMessage: '',
+        }
+    },
+
+    methods: {
+        async signInOrCreateUser() {
+
+            this.loading = true;
+            this.errorMessage = '';
+            try {
+                if (this.newUser) {
+                    await auth.createUserWithEmailAndPassword(this.email, this.password)
+                } else {
+                    await auth.signInWithEmailAndPassword(this.email, this.password)
+                }
+            } catch (error) {
+                this.errorMessage = error.message;
+            }
+
+            this.loading = false;
         }
     },
 })
