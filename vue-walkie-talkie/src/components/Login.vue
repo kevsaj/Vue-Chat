c04f404fe18bab441a775a7325c8fe280f928e7b
Kevin Sajan <kevsaj@gmail.com>
2021-04-13 23:49:45 -0400

    sign in with email

--- vue-walkie-talkie/src/components/Login.vue
+++ vue-walkie-talkie/src/components/Login.vue
@@ -2,6 +2,29 @@
     <aside>
         <h3>Sign in Anonymously</h3>
         <button @click="auth.signInAnonymously()" class="button is-dark">Sign In</button>
+
+        <div v-if="newUser">
+            <h3>Sign Up for a New Account</h3>
+            <a href="#" @click="newUser = false">Returning User?</a>
+        </div>
+
+        <div v-else>
+            <h3>Sign In with Email</h3>
+            <a href="#" @click="newUser = true"></a>
+        </div>
+
+        <label for="email">Email</label><br>
+        <input v-model="email" placeholder="email" type="email" class="input">
+
+        <label for="password">Password</label><br>
+        <input v-model="password" type="password" class="input">
+
+        <br>
+
+        <button>
+            {{ newUser ? 'Sign Up' : 'Login' }}
+        </button>
+
     </aside>
 </template>
 
@@ -11,7 +34,9 @@ import { auth } from '../firebase'
 export default ({
     data() {
         return {
-            auth,
+            newUser: false,
+            email: '',
+            password: '',
         }
     },
 })
