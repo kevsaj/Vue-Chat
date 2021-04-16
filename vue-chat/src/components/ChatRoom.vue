b4a6cc7ae6ecc35b4f127ebfe3f0048c08968ba1
Kevin Sajan <kevsaj@gmail.com>
2021-04-15 23:36:45 -0400

    Vue Router chatrooms

--- vue-chat/src/components/ChatRoom.vue
+++ vue-chat/src/components/ChatRoom.vue
@@ -1,16 +1,81 @@
 <template>
     <main>
         <h3>Welcome to ChatRoom {{ chatId }}</h3>
+
+        <User #user="{ user }">
+            <div v-if="user">
+                <ul>
+                    <li v-for="message of messages" :key="message.id">
+                        {{ message.text }}
+                    </li>
+                </ul>
+
+                <input v-model="newMessageText" class="input" />
+
+                <button
+                :disabled="!newMessageText || loading"
+                class="button is-success"
+                type="text"
+                @click="addMessage(user.uid)"
+                >Send</button>
+
+            </div>
+
+            <Login v-else />
+
+        </User>
+
     </main>
 </template>
 
 <script>
 
+import User from './User'
+import Login from './Login.vue';
+import { db } from '../firebase';
+
 export default ({
+    components: {
+        User,
+        Login
+    },
+    data() {
+        return {
+            newMessageText: '',
+            loading: false,
+            messages: [],
+        }
+    },
     computed: {
         chatId() {
             return this.$route.params.id;
         },
+        messagesCollection() {
+            return db.doc(`chats/${this.chatId}`).collection('messages');
+        },
     },
+    firestore() {
+        return {
+            messages: this.messagesCollection.orderBy('createdAt').limitToLast(10)
+        };
+    },
+    methods: {
+        async addMessage(uid) {
+
+            this.loading = true;
+
+            const { id: messageId } = this.messagesCollection.doc();
+
+            await this.messagesCollection.doc(messageId).set({
+                text: this.newMessageText,
+                sender: uid,
+                createdAt: Date.now(),
+            });
+
+            this.loading = false;
+            this.newMessageText = '';
+
+        }
+    }
 })
 </script>
