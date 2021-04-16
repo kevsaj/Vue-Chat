09e17d93e29f693b6a8c5d3b87b45c9ef096a597
Kevin Sajan <kevsaj@gmail.com>
2021-04-15 23:11:06 -0400

    link to chatrooms

---
+++ vue-chat/src/components/ChatRoom.vue
@@ -0,0 +1,16 @@
+<template>
+    <main>
+        <h3>Welcome to ChatRoom {{ chatId }}</h3>
+    </main>
+</template>
+
+<script>
+
+export default ({
+    computed: {
+        chatId() {
+            return this.$route.params.id;
+        },
+    },
+})
+</script>
